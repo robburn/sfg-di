41b6e189fd012158c6e885e46c08250e73c56dad
R <r@home.com>
2020-05-04 16:49:24 -0700

    Adding default Profile Annotation.

--- src/main/java/guru/springframework/sfgdi/services/I18NEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18NEnglishGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("I18NService")
 public class I18NEnglishGreetingService implements GreetingService{
     @Override
